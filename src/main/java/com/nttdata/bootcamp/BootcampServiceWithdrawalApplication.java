775ac3b12506989eda9017bd6b76f9d1d9c205d0
ant-lazo <ant.14sag@gmail.com>
2022-05-16 17:40:42 -0500

    microservice-withdrawal

---
+++ src/main/java/com/nttdata/bootcamp/BootcampServiceWithdrawalApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampServiceWithdrawalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampServiceWithdrawalApplication.class, args);
+	}
+
+}
